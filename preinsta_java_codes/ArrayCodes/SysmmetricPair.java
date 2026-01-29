1c3b73ca857a2951dae93c400d2f037a0b987188
Nimmana Monika <monikanimmana3366@gmail.com>
2026-01-29 22:06:53 +0530

    Improve SysmmetricPair logic and output
    Refactored sysmmetricpair method to include self-pairs and fix result handling.

--- preinsta_java_codes/ArrayCodes/SysmmetricPair.java
+++ preinsta_java_codes/ArrayCodes/SysmmetricPair.java
@@ -4,15 +4,16 @@ import java.util.*;
 public class SysmmetricPair {
     void sysmmetricpair(){
         int arr[][] = new int [][]{{10,20},{30,60},{20,10},{30,30}};
-        int result[][];
-        for(int i=0;i<arr.length;i++){
-            for(int j=0; j<arr.length;j++){
-                if(arr[i][0] == arr[j][1] && arr[i][1] == arr[j][0]){
-                    if(result[0]!= arr[i]){
-                        System.out.println(Arrays.toString(arr[i]));
-                    }
-                    result[i] = arr[i][0];
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i][0] == arr[i][1]){
+                System.out.println(Arrays.toString(arr[i]));
+            }
+            for(int j=i+1; j<n;j++){
+               if(arr[i][0] == arr[j][1] && arr[i][1] == arr[j][0]){
+                    System.out.println(Arrays.toString(arr[i]));  
                 }
+
             }
         }
     }
