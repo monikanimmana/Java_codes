4f9c500ac2c363d913a4a05d376268f9e2156312
monikanimmana <monikanimmana3366@gmail.com>
2026-03-12 10:05:23 +0530

    Adding LC_53 optimal  code

---
+++ LeetCodes/Arrays/LC_53.java
@@ -0,0 +1,27 @@
+package LeetCode.LeetCodes.Arrays;
+
+public class LC_53 {
+    public void Maximum_SubArray(){
+        int arr[] = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int sum = 0;
+        int max_profit = Integer.MIN_VALUE;
+        //Optimal Solution ------------------------------------------------------------------------
+        for (int i : arr){
+            sum += i;
+            if( sum > max_profit){
+                max_profit = sum;
+            }
+            if(sum <0){
+                sum =0;
+            }
+        }
+        System.out.println(max_profit);
+    }
+    public static void main(String[] args) {
+        LC_53 obj = new LC_53();
+        obj.Maximum_SubArray();
+    }
+    
+}
+
+
