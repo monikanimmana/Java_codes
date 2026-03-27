72a04a44c1621f1deabec04bfded592705337d0c
monikanimmana <monikanimmana3366@gmail.com>
2026-03-27 15:42:09 +0530

    adding Binary sum of arrayplay

---
+++ ArrayPlay/BinaryLongestSubarray.java
@@ -0,0 +1,24 @@
+package DSA_Java.ArrayPlay;
+
+import java.util.HashMap;
+
+public class BinaryLongestSubarray {
+    public int BinarySubarry(int arr[],int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max_length = 0;
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            if(map.get(1) == map.get(0)){
+                max_length = (map.get(1) + map.get(0));   
+            }
+
+        }
+        return max_length;
+    }
+    public static void main(String[] args) {
+        BinaryLongestSubarray obj = new BinaryLongestSubarray();
+        int arr[] = new int[]{0,1,0,1,1,0,1};
+        int k=0;
+        System.out.println(obj.BinarySubarry(arr, k));
+    }
+}
