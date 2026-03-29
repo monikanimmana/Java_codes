b7c7a74208e5ccb966dcee514e3d21db816f666b
monikanimmana <monikanimmana3366@gmail.com>
2026-03-29 08:19:34 +0530

    Adding Arrayplay palindome sum

---
+++ ArrayPlay/palindrome.java
@@ -0,0 +1,32 @@
+package DSA_Java.ArrayPlay;
+public class palindrome {
+   public boolean valid_palin(String str){
+    int i=0;
+    int j=str.length()-1;
+    while(i<j){
+        while(i<j && !Character.isLetterOrDigit(str.charAt(i))){
+            i++;
+        }
+
+        while(i<j && !Character.isLetterOrDigit(str.charAt(j))){
+            j--;
+        }
+        char left = Character.toLowerCase(str.charAt(i));
+        char right=Character.toLowerCase(str.charAt(j));
+
+        if(left != right){
+            return false;
+        }
+        i++;
+        j--;
+
+    }
+    return true;
+
+   } 
+   public static void main(String[] args) {
+    palindrome obj=new palindrome();
+    String str="A man, a plan, a canal: Panama";
+    System.out.println(obj.valid_palin(str));
+   }
+}
