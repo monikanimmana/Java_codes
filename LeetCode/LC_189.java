a48ebec40110abb4e4f85c9c44f6c49ef029729a
monikanimmana <monikanimmana3366@gmail.com>
2026-04-01 14:01:45 +0530

    Adding leetcode number 189

--- LeetCode/LC_189.java
+++ LeetCode/LC_189.java
@@ -1,22 +1,32 @@
 import java.util.Arrays;
 
 public class LC_189 {
-    public int[] rotateArray(int nums[] , int k){
+    public void rotateArray(int nums[],int k){
         int n=nums.length;
-        int j=k-1;
-        for(int i=n-1;i>=n-k;i--){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            j--;
+        k=k%n;
 
+        reverse(nums,0,n-1);
+
+        reverse(nums,0,k-1);
+    }
+
+    public int[] reverse(int nums[],int start , int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
         }
         return nums;
+
     }
+    
     public static void main(String[] args) {
         LC_189 obj=new LC_189();
         int nums[] = new int[]{1,2,3,4,5,6,7};
         int k=3;
-        System.out.println(Arrays.toString(obj.rotateArray(nums, k)));
+        obj.rotateArray(nums, k);
+        System.out.println(Arrays.toString(nums));
     }
 }
