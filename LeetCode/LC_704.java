7a4ddb431ae12c38e6490c9feec1ac98832559f0
monikanimmana <monikanimmana3366@gmail.com>
2026-04-02 22:19:25 +0530

    Adding leetcode number 704

--- LeetCode/LC_704.java
+++ LeetCode/LC_704.java
@@ -2,7 +2,7 @@ public class LC_704{
     public int search(int[] nums, int target) {
         int left=0;
         int right=(nums.length)-1;
-        while(left<right){
+        while(left<=right){
             int mid=(left+right)/2;
             if(nums[mid]==target){
                 return mid;
