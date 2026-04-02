562ae25aafa4e8e63795266d3274677b20e1b95e
monikanimmana <monikanimmana3366@gmail.com>
2026-04-02 08:21:51 +0530

    adding cyclicarray sum in ArrayPlay

---
+++ ArrayPlay/cyclicReplacement.java
@@ -0,0 +1,29 @@
+
+package DSA_Java.ArrayPlay;
+import java.util.Arrays;
+
+public class cyclicReplacement{
+    public int[] cyclicArray(int nums[], int k){
+        int n=nums.length;
+        int i=0;
+        int j=0;
+        int current_value=nums[i];
+        while(j<n){
+            int index=(i+k)%n;
+            int next_value=nums[index];
+            nums[index]=current_value;
+            i=index;
+            current_value=next_value;
+            j++;
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        cyclicReplacement obj=new cyclicReplacement();
+        int nums[] = new int[]{1,2,3,4,5,6,7};
+        int k=3;
+        System.out.println(Arrays.toString(obj.cyclicArray(nums, k)));
+
+    }
+    
+}
