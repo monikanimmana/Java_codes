2f7e00f76617c49b4c43c8c979270d649c6b7966
monikanimmana <monikanimmana3366@gmail.com>
2026-04-06 07:45:03 +0530

    Adding leetcode 152 intuitive method

--- LeetCode/LC_152.java
+++ LeetCode/LC_152.java
@@ -27,3 +27,17 @@ public class LC_152 {
     }
 }
 
+
+//intuitive method
+// int prefix=1;
+// int suffix=1;
+// for(int i=0;i<nums.length;i++){
+//     if(prefix==0) --> prefix=1;
+//     if(suffix==0) --> suffix=1;
+
+//     prefix *=nums[i];
+//     suffix *=nums[n-i-1];
+
+//     maxi=Math.max(maxi, Math.max(prefix, suffix));
+// }
+// return maxi;
