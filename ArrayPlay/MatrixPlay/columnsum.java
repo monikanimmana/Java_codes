6b16a9e9a773d5b6f7cd9bf8831433601a80e61b
monikanimmana <monikanimmana3366@gmail.com>
2026-04-16 21:15:19 +0530

    martix column sum

--- ArrayPlay/MatrixPlay/columnsum.java
+++ ArrayPlay/MatrixPlay/columnsum.java
@@ -1,12 +1,15 @@
 public class columnsum {
     public int colsum(int nums[][]){
         int sum=0;
+        int max_sum=Integer.MIN_VALUE;
         for(int j=0;j<nums[0].length;j++){
             for(int i=0;i<nums.length;i++){
                 sum += nums[i][j];
             }
+            max_sum=Math.max(max_sum, sum);
+            sum=0;
         }
-        return sum;
+        return max_sum;
     }
     public static void main(String[] args) {
        columnsum obj=new columnsum();
