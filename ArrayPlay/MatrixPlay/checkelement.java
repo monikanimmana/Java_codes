7043f9a3eaaddb715d16f9f367f0ab75b7ba91dd
monikanimmana <monikanimmana3366@gmail.com>
2026-04-16 21:18:04 +0530

    martix find element

--- ArrayPlay/MatrixPlay/checkelement.java
+++ ArrayPlay/MatrixPlay/checkelement.java
@@ -13,7 +13,7 @@ public class checkelement {
     public static void main(String[] args) {
        checkelement obj=new checkelement();
        int nums[][]=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
-       int k=6;
+       int k=10;
        System.out.println(obj.find(nums,k));
     }
 }
