b613dae6eb6fc1898459302fab01c226f8db0396
monikanimmana <monikanimmana3366@gmail.com>
2026-04-16 20:24:42 +0530

    martix count  element

---
+++ ArrayPlay/MatrixPlay/countelement.java
@@ -0,0 +1,16 @@
+public class countelement{
+    public int elementcount(int nums[][]){
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums[i].length;j++){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+       matrixsum obj=new matrixsum();
+       int nums[][]=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+       System.out.println(obj.sum(nums));
+    }
+}
