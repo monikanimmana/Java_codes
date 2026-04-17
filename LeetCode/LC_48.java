fa4636b83f70eaf49f6b8f6c47a98a83d92244ca
monikanimmana <monikanimmana3366@gmail.com>
2026-04-17 20:30:35 +0530

    Leetcode number 48

--- LeetCode/LC_48.java
+++ LeetCode/LC_48.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-public class LC_867 {
+
+public class LC_48 {
     public int[][] transposeMatrix(int matrix[][]){
         for(int i=0;i<matrix.length;i++){
             for(int j=i+1;j<matrix.length;j++){
@@ -10,11 +11,23 @@ public class LC_867 {
         }
         return matrix; 
     }
+    public int[][] reversematrix(int matrix[][]){
+        int m=matrix[0].length;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<m/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][m-j-1];
+                matrix[i][m-j-1]=temp;
+            }
+        }
+        return matrix;
+    }
 
     public static void main(String[] args) {
-        LC_867 obj=new LC_867();
+        LC_48 obj=new LC_48();
         int matrix[][]=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
         obj.transposeMatrix(matrix);
+        obj.reversematrix(matrix);
         System.out.println(Arrays.deepToString(matrix));
     }
 }
