d74a73bdeb323defca376b79faf716345421770e
monikanimmana <monikanimmana3366@gmail.com>
2026-04-23 10:51:48 +0530

    reverse array using recursion

--- ArrayPlay/reverse_array.java
+++ ArrayPlay/reverse_array.java
@@ -1,3 +1,15 @@
 public class reverse_array {
-    
+    public void array(int i,int n , int arr[]){
+        if(i>n){
+            return;
+        }
+        array(i+1,n,arr);
+        System.out.println(arr[i]);
+    }
+    public static void main(String[] args) {
+        reverse_array obj=new reverse_array();
+        int arr[]=new int[]{1,2,3,4,5};
+        obj.array(0, arr.length-1, arr);
+
+    }
 }
