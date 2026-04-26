9ce7129f587b91d955c352bd398dcbd698aa624d
monikanimmana <monikanimmana3366@gmail.com>
2026-04-26 16:59:40 +0530

    bubblesort sorting

--- ArrayPlay/Sorting/bubble_sort.java
+++ ArrayPlay/Sorting/bubble_sort.java
@@ -1,14 +1,14 @@
 
 import java.util.Arrays;
 public class bubble_sort {
-    public int[] sort(int nums[]){
+    public int[] sortelements(int nums[]){
         int n=nums.length;
         for(int i=n-1;i>=1;i--){
             for(int j=0;j<=i-1;j++){
-                if(nums[j]>nums[j++]){
+                if(nums[j]>nums[j+1]){
                     int temp=nums[j];
-                    nums[j]=nums[j++];
-                    nums[j++]=temp;
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
                 }
             }
         }
@@ -17,7 +17,7 @@ public class bubble_sort {
     public static void main(String[] args) {
         bubble_sort obj=new bubble_sort();
         int nums[]=new int[]{13,24,2,56,34,26,1};
-        System.out.println(Arrays.toString(obj.sort(nums)));
+        System.out.println(Arrays.toString(obj.sortelements(nums)));
     }
     
 }
