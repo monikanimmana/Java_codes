7fbb17741addfb03080a1da3c5c3b111a1728365
monikanimmana <monikanimmana3366@gmail.com>
2026-04-26 16:53:25 +0530

    bubblesort sorting

---
+++ ArrayPlay/Sorting/bubble_sort.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+public class bubble_sort {
+    public int[] sort(int nums[]){
+        int n=nums.length;
+        for(int i=n-1;i>=1;i--){
+            for(int j=0;j<=i-1;j++){
+                if(nums[j]>nums[j++]){
+                    int temp=nums[j];
+                    nums[j]=nums[j++];
+                    nums[j++]=temp;
+                }
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        bubble_sort obj=new bubble_sort();
+        int nums[]=new int[]{13,24,2,56,34,26,1};
+        System.out.println(Arrays.toString(obj.sort(nums)));
+    }
+    
+}
