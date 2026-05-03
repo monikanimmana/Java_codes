c26c258b4fc58ea3fe6eb0374d20c692a9aeec52
monikanimmana <monikanimmana3366@gmail.com>
2026-05-03 10:15:23 +0530

    leetcode number 90

--- LeetCode/LC_90.java
+++ LeetCode/LC_90.java
@@ -1,24 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 public class LC_90 {
     public List<List<Integer>> subsetWithDup(int []nums){
         int index=0;
         List<List<Integer>> result=new ArrayList<>();
         List<Integer> list=new ArrayList<>();
+        Arrays.sort(nums);
         checksubset(nums , index , list, result);
         return result;
     }
     public void checksubset(int []nums , int index , List<Integer> list , List<List<Integer>> result ){
-        if(index==nums.length){
-            if(!result.contains(list)){
-                result.add(new ArrayList<>(list));
-            }
-            return;
+        result.add(new ArrayList<>(list));
+        for(int i=index;i<nums.length;i++){
+            if(i>index&&nums[i]==nums[i-1]) continue;
+
+            list.add(nums[i]);
+            checksubset(nums, i+1, list, result);
+            list.remove(list.size()-1);
+            
         }
-        list.add(nums[index]);
-        checksubset(nums, index+1, list, result);
-        list.remove(list.size()-1);
-        checksubset(nums, index+1, list, result);
     }
     public static void main(String[] args) {
         LC_90 obj=new LC_90();
