17432219f0c0f2d6f211bb157518e0c0943965f2
monikanimmana <monikanimmana3366@gmail.com>
2026-05-03 10:33:57 +0530

    leetcode number 40

--- LeetCode/LC_40.java
+++ LeetCode/LC_40.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 public class LC_40 {
-    
+    public List<List<Integer>> combination2(int candidate[], int target){
+        List<List<Integer>> result=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        int index=0;
+        checksum(candidate,target,index,list,result);
+        return result;
+    }
+    public void checksum(int candidate[], int target , int index , List<Integer> list , List<List<Integer>> result){
+        if(target == 0){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=index;i<candidate.length;i++){
+            if(candidate[i] <= target){
+                list.add(candidate[i]);
+                checksum(candidate, target-candidate[i], i+1, list, result);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        LC_40 obj=new LC_40();
+        int candidate[] = new int[]{10,1,2,7,6,1,5};
+        int target=8;
+        System.out.println(obj.combination2(candidate,target));
+    }
 }
